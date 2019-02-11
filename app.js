033a314fffc1e5fa38494bfbb58c5b8353e97849
Adam Stuller <adam.stuller@protonmail.com>
2019-02-11 15:20:30 +0100

    serving first html file

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/' , (req , res , next) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<Body><h1>Hello Karesko</h1></Body>');
+    res.end();
+});
+
 app.listen(4000);
